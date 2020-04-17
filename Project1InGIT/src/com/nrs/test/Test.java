dc00f7a251538a14d22caa390f52c9f587d863a9
TL <TL@gmail.com>
2020-04-16 23:34:15 -0500

    Test class is added by Developer1

---
+++ Project1InGIT/src/com/nrs/test/Test.java
@@ -0,0 +1,12 @@
+package com.nrs.test;
+
+import com.nrs.utility.Employee;
+
+public class Test {
+
+public static void main(String[] args) {
+	System.out.println("Test class main method...");
+	Employee e=new Employee();
+	e.m1();
+}
+}
